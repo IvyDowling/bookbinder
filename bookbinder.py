68522fcbb30618485666ff9f75d3470a99048ca4
MichaelIV <michaeldowlingiv@gmail.com>
2017-01-17 13:33:05 -0500

    added userguidepdf and supported css styles list

--- bookbinder.py
+++ bookbinder.py
@@ -5,11 +5,18 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.pagesizes import landscape
 
 
+supportedStyles = [
+    "font",
+    "background-color",
+    "rotate",
+    "margin"
+]
+
 def reader(title):
     with open(title) as data_file:
         data = json.load(data_file)
 
-    #document has style obj and pages array
+    #document has style obj and pages obj
     style = data["style"]
     pages = data["pages"]
     page_style = [style if p is {} else inheritGlobalStyle(style,p)]
@@ -21,10 +28,18 @@ def reader(title):
 
 def writer(title, book):
     filename = title + '.pdf'
-    c = canvas.Canvas(filename, pagesize=landscape(letter))
-    #c.setFont()
-    #c.drawString(x,y,text)
-    c.showPage()
+    c = canvas.Canvas(filename, pagesize=letter)
+    width, height = letter
+    for pg in book:
+        #setup style
+        for style in pg.style:
+            css_to_reportlab(c, style)
+
+        #content
+        for cont in pg.content:
+
+        #ends page
+        c.showPage()
     print("writing to file: ", title, ".pdf")
     c.save()
 
@@ -32,6 +47,18 @@ def writer(title, book):
 def inheritGlobalStyle(glob, loc):
     for key in vars(newStyle).keys():
         glob[key] = newStyle[key]
+
+
+def css_to_reportlab(canvas, css):
+    #c.setFont(psfontname, size)
+    #canvas.getAvailableFonts()
+    #c.drawString(x,y,text)
+    #c.setFillColor(red)
+    #canvas.rotate(theta)
+    if css is supportedStyles[0]: #font: (size px) (face)
+    if css is supportedStyles[1]: #background-color: (rgb)
+    if css is supportedStyles[2]: #rotate (degrees)
+    if css is supportedStyles[3]: #margin (top) (right) (bottom) (left) 
 
 
 class Page:
@@ -51,6 +78,9 @@ class Page:
 
 if __name__ == "__main__":
     if len(sys.argv) is 2:
-        reader(sys.argv[1])
+        if(sys.argv[1] is "--help" or sys.argv[1] is "-h"):
+            print("supported styles: ", supportedStyles)
+        else:
+            reader(sys.argv[1])
     else:
         reader("book.json")
